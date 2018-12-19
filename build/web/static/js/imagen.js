84f5f8098dba7db060c41c656ca65f83d5c1944d
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2018-12-19 19:54:23 -0300

    add resize

--- build/web/static/js/imagen.js
+++ build/web/static/js/imagen.js
@@ -1,4 +1,5 @@
 let archivos = [];
+let enviarArchivos = [];
 class Imagen {
 
     static select(files) {
@@ -16,7 +17,6 @@ class Imagen {
 
     static dropImg(e) {
         e.preventDefault();
-        console.log(e.dataTransfer.files);
         Imagen.convertirImg(e.dataTransfer.files);
     }
 
@@ -29,14 +29,15 @@ class Imagen {
 
                 let fileReader = new FileReader();
 
-                if (files[i].type.match(/^image\//) && files[i].size > 1048576) {
+                if (files[i].type.match(/^image\//) && files[i].size > 512000) {
                     temp +=
-                            `<p id="fu-${i}">${files[i].name} el archivo tiene mas de 1MB</p>`;
+                            `<p id="fu-${i}">${files[i].name} el archivo tiene mas de 500Kb</p>`;
                     document.querySelector('#panelU').innerHTML = temp;
 
                     continue;
 
                 }
+
                 fileReader.readAsDataURL(files[i]);
 
 
@@ -45,13 +46,19 @@ class Imagen {
 
                 document.querySelector('#panelU').innerHTML = temp;
 
+                document.querySelector(".spin").classList.add("spinner");
+
                 fileReader.addEventListener("load", e => {
 
                     archivos.push(e.target.result);
-                    document.querySelector(`#fu-${i}`).innerText = `${files[i].name} loaded`;
+                    enviarArchivos.push(e.target.result);
 
+                    document.querySelector(`#fu-${i}`).innerText = `${files[i].name} loaded`;
+                    document.querySelector(".spin").classList.remove("spinner");
                 });
+
             }
+
         } else {
             let template = '';
             if (files.length > 5) {
@@ -68,6 +75,52 @@ class Imagen {
 
     }
 
+    static rango() {
+        const r = document.querySelector('#maxHeight');
+        document.querySelector('#valorRango').innerText = r.value;
+        return r.value;
+    }
+
+    static redimensionar(e) {
+        e.preventDefault();
+        if (archivos) {
+            const size = Imagen.rango();
+            Imagen.resize(size);
+
+        }
+
+    }
+
+    static resize(size) {
+
+        for (let i = 0; i < archivos.length; i++) {
+
+            let image = new Image();
+            image.src = archivos[i];
+            
+            document.querySelector(".spin").classList.add("spinner");
+            
+            image.onload = () => {
+
+                let width = image.width;
+                let height = image.height;
+
+                let canvas = document.createElement('canvas');
+
+                let escala = size / height;
+                canvas.height = size;
+                canvas.width = width * escala;
+
+                canvas.getContext('2d').drawImage(image, 0, 0, width * escala, size);
+                enviarArchivos[i] = canvas.toDataURL();
+
+                document.querySelector(`#fu-${i}`).innerText = 'resized completed!!';
+                document.querySelector(".spin").classList.remove("spinner");
+            };
+
+        }
+    }
+
     static async traer() {
         /*
          const url = 'UploadImgServer?&q=';
@@ -75,25 +128,27 @@ class Imagen {
          const response = await Http.get(url + JSON.stringify(param));
          const img = await JSON.parse(response.img);
          */
-
-        const url = 'UploadImgServer';
-        const response = await Http.get(url);
-
-        let template =
-                `${response.map(i =>
-                        `<img src="${i.img}" style = width:100px alt="img"/>`
-                ).join(' ')}`;
-
-        document.querySelector('#panel').innerHTML = template;
+        
+         const url = 'UploadImgServer';
+         const response = await Http.get(url);
+         
+         let template =
+         `${response.map(i =>
+         `<img src="${i.img}" style = width:100px alt="img"/>`
+         ).join(' ')}`;
+         
+         document.querySelector('#panel').innerHTML = template;
+     
 
     }
 
     static async subir() {
-        if (archivos && archivos.length > 0) {
+        if (enviarArchivos && enviarArchivos.length > 0) {
             const url = 'UploadImgServer';
             //const img = JSON.stringify(archivos);
-            const response = await Http.post(url, archivos);
+            const response = await Http.post(url, enviarArchivos);
             archivos = [];
+            enviarArchivos = [];
             console.log(response);
 
         } else
